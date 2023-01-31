a48e2105fb0cbe659b81c65644a464ef90bc6b19
mq61520 <qfncore@gmail.com>
2023-01-31 14:16:59 +0700

    fix header

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -2,15 +2,17 @@ import { useState } from 'react';
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome/index';
+import { faBagShopping } from '@fortawesome/free-solid-svg-icons/index';
 
-import Button from '~/components/Button';
 import styles from './Header.module.scss';
+import Button from '~/components/Button';
 import Dropdown from '~/components/Dropdown';
 
 const cn = classNames.bind(styles);
 
 function Header() {
-   const [currentUser, setCurrentUser] = useState('idsbbvihvuih');
+   const [currentUser, setCurrentUser] = useState('sadvsdv');
 
    const handerLogout = () => {
       localStorage.removeItem('name');
@@ -24,45 +26,62 @@ function Header() {
                2hand Store
             </div>
 
-            <div className={cn('account')}>
-               {currentUser ? (
-                  <Tippy
-                     interactive
-                     render={(attrs) => (
-                        <div className={cn('content')} tabIndex="-1" {...attrs}>
-                           <Dropdown>
-                              <div className={cn('account-tool')}>
-                                 <Button onlytext to={'/profile'}>
-                                    Profile
-                                 </Button>
-                              </div>
-
-                              <div className={cn('account-tool')}>
-                                 <Button onlytext onClick={handerLogout}>
-                                    Logout
-                                 </Button>
-                              </div>
-                           </Dropdown>
+            <div className={cn('flex-right-header')}>
+               <div className={cn('cart')}>
+                  {currentUser ? (
+                     <>
+                        <div className={cn('cart-btn')}>
+                           <Button onlytext to={'/cart'}>
+                              <FontAwesomeIcon className={cn('cart-icon')} icon={faBagShopping} />
+                              <div className={cn('cart-flag')}>99</div>
+                           </Button>
                         </div>
-                     )}
-                  >
-                     <div className={cn('account-name')}>{currentUser}</div>
-                  </Tippy>
-               ) : (
-                  <div className={cn('account-actions')}>
-                     <div className={cn('action')}>
-                        <Button border to={'/register'}>
-                           Sign up
-                        </Button>
-                     </div>
+                     </>
+                  ) : (
+                     <></>
+                  )}
+               </div>
 
-                     <div className={cn('action')}>
-                        <Button border to={'/login'}>
-                           Sign in
-                        </Button>
+               <div className={cn('account')}>
+                  {currentUser ? (
+                     <Tippy
+                        interactive
+                        render={(attrs) => (
+                           <div className={cn('content')} tabIndex="-1" {...attrs}>
+                              <Dropdown>
+                                 <div className={cn('account-tool')}>
+                                    <Button onlytext to={'/profile'}>
+                                       Profile
+                                    </Button>
+                                 </div>
+
+                                 <div className={cn('account-tool')}>
+                                    <Button onlytext onClick={handerLogout}>
+                                       Logout
+                                    </Button>
+                                 </div>
+                              </Dropdown>
+                           </div>
+                        )}
+                     >
+                        <div className={cn('account-name')}>{currentUser}</div>
+                     </Tippy>
+                  ) : (
+                     <div className={cn('account-actions')}>
+                        <div className={cn('action')}>
+                           <Button border to={'/register'}>
+                              Sign up
+                           </Button>
+                        </div>
+
+                        <div className={cn('action')}>
+                           <Button border to={'/login'}>
+                              Sign in
+                           </Button>
+                        </div>
                      </div>
-                  </div>
-               )}
+                  )}
+               </div>
             </div>
          </div>
       </div>
